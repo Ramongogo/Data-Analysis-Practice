ebced3a17b5c3ed710bb147cc31fb1e1ede1d0fe
Ramon <ramon8020748@gmail.com>
2024-07-19 13:22:13 +0800

    Revised

--- Revenue_Prediction.py
+++ Revenue_Prediction.py
@@ -1,4 +1,3 @@
-#%%
 #Importing data and required packages
 import pandas as pd
 import numpy as np
@@ -17,12 +16,13 @@ from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 from sklearn.preprocessing import LabelEncoder
 import optuna
+
 data = pd.read_csv("C:/Users/USER/Downloads/data/Restaurant_revenue (1).csv", encoding = 'utf8')
 df = pd.DataFrame(data)
 pd.set_option('display.max_columns',None)
+
 #Checking missing value
 print(df.isnull().sum())
-#%%
 
 #Obseving correlation between each columns 
 dummies = pd.get_dummies(df['Cuisine_Type'] , drop_first=False).astype(int)
@@ -31,8 +31,7 @@ df = df.drop(['Cuisine_Type'], axis=1)
 matrix = df.corr()
 sns.heatmap(matrix, annot = True, cmap = 'coolwarm')
 plt.show()
-#Number of Customers has a strong postive correlation of 0.75 with Montly revenue, 
-#while Menu price and Marketing spend have relatively weak correlations of 0.26 and 0.27 with Monthly Revenue. The others columns have no significant relationship with Monthly revenue.
+
 #Removing outliers
 for i in range(len(df.columns)):
     Q1 = np.percentile(df.iloc[:,i], 25, method = 'midpoint')
@@ -42,6 +41,7 @@ for i in range(len(df.columns)):
     upper = Q3 + 1.5*IQR
     outliers = (df.iloc[:,i] > upper) | (df.iloc[:,i] < lower)
 df = df[~outliers]
+
 #Model Training
 x = df[['Number_of_Customers','Menu_Price','Marketing_Spend']]
 #x = df.drop(['Monthly_Revenue'], axis = 1)
@@ -49,12 +49,14 @@ y = df['Monthly_Revenue']
 scaler = StandardScaler()
 x = scaler.fit_transform(x)
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=88)
+
 #Define evaluation function
 def evaluation(real, predicted):
     mae = mean_absolute_error(real,predicted)
     mse = mean_squared_error(real,predicted)
     r2 = r2_score(real,predicted)
     return mae, mse, r2
+
 #Define training and tuning model function
 param_grid = {  'Linear Regression':{},'Lasso':{'alpha':[0.01,0.1,1,10,100]},'Ridge':{'alpha':[0.01,0.1,1,10,100]},
                 'Decision Tree':{'max_depth':[10,20,30],
@@ -81,9 +83,10 @@ param_grid = {  'Linear Regression':{},'Lasso':{'alpha':[0.01,0.1,1,10,100]},'Ri
 models = {'Linear Regression':LinearRegression(),"Lasso":Lasso(),"Ridge":Ridge(),"Decision Tree":DecisionTreeRegressor(),
               "Random Forest Regressor":RandomForestRegressor(),"XGBRegressor":XGBRegressor(),"CatBoosting Regressor":CatBoostRegressor(verbose=False),
               "AdaBoostRegressor":AdaBoostRegressor()}
+
 results_before = []
 results_after = []
-#Before tuning
+#Before tuning resluts
 for model_name, model in models.items():
     model.fit(x_train,y_train)
     y_train_pred = model.predict(x_train)
@@ -95,7 +98,8 @@ for model_name, model in models.items():
                             'Train MSE':model_train_mse,'Train R2':model_train_r2,
                             'Test MAE':model_test_mae,'Test MSE':model_test_mse,
                             "Test R2":model_test_r2,'cv_scores':cv_scores.mean()})
-#After tuning
+
+#After tuning results
     param_grid = param_grid.get(model_name,{})
     if param_grid:
         grid_search = GridSearchCV(estimator = model, param_grid = param_grid,cv = 5, scoring = 'r2', n_jobs=-1)
@@ -113,16 +117,10 @@ for model_name, model in models.items():
                                 "Train MSE":train_mse_tuned,"Train R2":train_r2_tuned,
                                 'Test MAE':test_mae_tuned,'Test MSE':test_mse_tuned,
                                 'Test R2':test_r2_tuned,'cv_scores':cv_scores_tuned.mean()})
-    #print(f"Model: {model_name}")
-    #print('Training Performance Before Tuning')
-    #print(f'MAE = {model_train_mae}, MSE = {model_train_mse}, R2 = {model_train_r2}\n')
-    #print('Testing Performance Before Tuning')
-    #print(f'MAE = {model_test_mae}, MSE = {model_test_mse}, R2 = {model_test_r2}')
-    #print('Training Performance After Tuning')
-    #print(f'MAE = {train_mae_tuned}, MSE = {train_mse_tuned}, R2 = {train_r2_tuned}\n')
-    #print('Testing Performance After Tuning')
-    #print(f'MAE = {test_mae_tuned}, MSE = {test_mse_tuned}, R2 = {test_r2_tuned}')
-    #print('-' * 40)
+Comparison_before= pd.DataFrame(results_before).sort_values(by = ['Test R2'],ascending=False) 
+Comparison_after = pd.DataFrame(results_after).sort_values(by = ['Test R2'],ascending=False)
+
+#Optuna tuning for the best model
 def objective(trial):
     model = Lasso(alpha = trial.suggest_loguniform('alpha', 0.001, 100))
     score = cross_val_score(model, x_train, y_train, cv=10, scoring='neg_mean_squared_error')
@@ -133,15 +131,20 @@ best_params = study.best_params
 best_lasso = Lasso(**best_params)
 best_lasso.fit(x_train, y_train)
 y_pred_lasso = best_lasso.predict(x_test)
+y_pred_train_lasso = best_lasso.predict(x_train)
 lasso_test_mae, lasso_test_mse, lasso_test_r2 = evaluation(y_test, y_pred_lasso)
-Comparison_before= pd.DataFrame(results_before).sort_values(by = ['Test R2'],ascending=False)
-Comparison_after = pd.DataFrame(results_after).sort_values(by = ['Test R2'],ascending=False)
+lasso_train_mae, lasso_train_mse, lasso_train_r2 = evaluation(y_train, y_pred_train_lasso)
+cv_scores_lasso = cross_val_score(best_lasso, x_train, y_train, cv=10, scoring='r2', n_jobs=-1)
+lasso_data = ['Best_Lasso', lasso_train_mae,lasso_train_mse,lasso_train_r2,lasso_test_mae,lasso_test_mse,lasso_test_r2,cv_scores_lasso.mean()]
+Comparison_after.loc[8] = lasso_data
+Comparison_after = Comparison_after.sort_values(by = ['Test R2'],ascending=False)
+#Results comparison
 print('Before tuning')
 print(Comparison_before)
 print('After tuning')
 print(Comparison_after)
-print(f'Lasso result : MSE = {lasso_test_mse},R2 = {lasso_test_r2}, Intercept = {best_lasso.intercept_}, Coef : {best_lasso.coef_}')
-print('test2')
+print(f'Lasso optuna result : MSE = {lasso_test_mse},R2 = {lasso_test_r2}, Intercept = {best_lasso.intercept_}, Coef : {best_lasso.coef_}')
 
 
-# %%
+
+
